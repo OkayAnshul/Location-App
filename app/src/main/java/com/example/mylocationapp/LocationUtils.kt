07f56c4a62f45c93d492360284b8726a607609db
Anshul Kumar <anshulisokay@gmail.com>
2024-07-27 07:54:15 +0530

    Add Check Function

--- app/src/main/java/com/example/mylocationapp/LocationUtils.kt
+++ app/src/main/java/com/example/mylocationapp/LocationUtils.kt
@@ -6,23 +6,17 @@ import android.content.pm.PackageManager
 import androidx.core.content.ContextCompat
 
 class LocationUtils(val context:Context) {
-
     //Fn to check the current permissionState
 
     fun hasLocationPermission(context: Context):Boolean{
-        if(
-            (ContextCompat.checkSelfPermission(context,
+        return ((ContextCompat.checkSelfPermission(context,
             Manifest.permission.ACCESS_FINE_LOCATION)==
-                    PackageManager.PERMISSION_GRANTED
-                    )
-            &&
-            (ContextCompat.checkSelfPermission(context,
-                Manifest.permission.ACCESS_FINE_LOCATION)==
-                    PackageManager.PERMISSION_GRANTED
-                    )
-            )
-            return true
-        else
-            return false
+                PackageManager.PERMISSION_GRANTED
+                )
+                &&
+                (ContextCompat.checkSelfPermission(context,
+                    Manifest.permission.ACCESS_FINE_LOCATION)==
+                        PackageManager.PERMISSION_GRANTED
+                        ))
     }
 }
