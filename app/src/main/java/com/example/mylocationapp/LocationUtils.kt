83411a3107f3961d498ec0cbc1013c4b54765e95
Anshul Kumar <anshulisokay@gmail.com>
2024-07-28 20:16:31 +0530

    Add CoordinatesDecoder

--- app/src/main/java/com/example/mylocationapp/LocationUtils.kt
+++ app/src/main/java/com/example/mylocationapp/LocationUtils.kt
@@ -4,6 +4,8 @@ import android.Manifest
 import android.annotation.SuppressLint
 import android.content.Context
 import android.content.pm.PackageManager
+import android.location.Address
+import android.location.Geocoder
 import android.os.Looper
 import androidx.core.content.ContextCompat
 import com.google.android.gms.location.FusedLocationProviderClient
@@ -12,6 +14,8 @@ import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationResult
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.location.Priority
+import com.google.android.gms.maps.model.LatLng
+import java.util.Locale
 
 class LocationUtils(val context:Context) {
 
@@ -47,5 +51,17 @@ class LocationUtils(val context:Context) {
                     Manifest.permission.ACCESS_FINE_LOCATION)==
                         PackageManager.PERMISSION_GRANTED
                         ))
+    }
+    fun reverseGeocoderLocation(location:LocationData):String{
+        val geocoder=Geocoder(context,Locale.getDefault())
+        val coordinates =LatLng(location.latitude,location.longitude)
+        val addressess:MutableList<Address>?=geocoder.getFromLocation(coordinates.latitude,coordinates.longitude,1)
+        return if(addressess?.isNotEmpty()==true) {
+            addressess[0].getAddressLine(0)
+        }
+        else
+            "Address not found"
+        }
+
     }
 }
