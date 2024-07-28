72ef3eeccd29de918d868817a48c156056e86f21
Anshul Kumar <anshulisokay@gmail.com>
2024-07-28 06:41:59 +0530

    Add locationCallBack

--- app/src/main/java/com/example/mylocationapp/LocationUtils.kt
+++ app/src/main/java/com/example/mylocationapp/LocationUtils.kt
@@ -1,13 +1,42 @@
 package com.example.mylocationapp
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.pm.PackageManager
+import android.os.Looper
 import androidx.core.content.ContextCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
 
 class LocationUtils(val context:Context) {
-    //Fn to check the current permissionState
 
+    private val _fusedLocationClient:FusedLocationProviderClient
+    =LocationServices.getFusedLocationProviderClient(context)
+
+    @SuppressLint("MissingPermission")
+    fun requestLocationUpdates(viewModel: LocationViewModel)
+    {
+        val locationCallback=object :LocationCallback(){
+            override fun onLocationResult(locationResult: LocationResult) {
+                super.onLocationResult(locationResult)
+                locationResult.lastLocation?.let{
+                    val location =LocationData(longitude = it.longitude,
+                        latitude = it.latitude)
+                    viewModel.updateLocation(location)
+                }
+            }
+        }
+        val locationRequest=LocationRequest.
+        Builder(Priority.PRIORITY_HIGH_ACCURACY,1000).build()
+        _fusedLocationClient.requestLocationUpdates(locationRequest,locationCallback, Looper.getMainLooper())
+    }
+
+    //Fn to check the current permissionState
     fun hasLocationPermission(context: Context):Boolean{
         return ((ContextCompat.checkSelfPermission(context,
             Manifest.permission.ACCESS_FINE_LOCATION)==
